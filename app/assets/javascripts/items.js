e3fb212fa74c42e016256e42b5a9e4d6e9272687
Joe <modulusman@gmail.com>
2017-08-04 16:47:25 -0700

    Fucked'

---
+++ app/assets/javascripts/items.js
@@ -0,0 +1,32 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+// $("#imgSmall").click(function(){		                                       $("#imgBig").attr("src","http://www.freeimageslive.com/galleries/space/earth/pics/a17_h_148_22718.gif");
+// $("#overlay").show();
+// $("#overlayContent").show();
+// });
+//
+// $("#imgBig").click(function(){
+//   $("#imgBig").attr("src", "");
+//   $("#overlay").hide();
+//   $("#overlayContent").hide();
+// });
+
+
+
+
+
+// <script>
+// $("#imgSmall").click(function(){
+//   $("#imgBig").attr("src","<%= image_url(image.url(:large)) %>");
+//   $(".overlay").show();
+//   $("#overlayContent").show();
+// });
+//
+// $("#imgBig").click(function(){
+//   $("#imgBig").attr("src", "<%= image_tag(image.url(:large)) %>");
+//   $(".overlay").hide();
+//   $("#overlayContent").hide();
+// });
+// </script>
