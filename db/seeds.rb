12bcdacd3f90e8e844a8db15d210b4d0d2ceff8d
Amy Dyson <amy.dyson@mac.com>
2017-08-07 16:40:39 -0700

    Moves images from local desktop to app folder named test so that images are accessible to all.

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,11 @@ user = User.create!(email: 'amy.dyson@mac.com', password: 'jstreet', password_co
 sale = Sale.create!(street: '13162 Capstone Drive', city: 'San Diego', state: 'California', zip: '92130', date: '08/24/2017', description: 'Moving soon and everything must go.', user_id: user.id )
 
 item = Item.new(item_name: 'Shoes', item_description: 'Barely used brown Dansko clogs', price: '$25', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/dankso.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/dankso.jpg")
 item.save
 
 item = Item.new(item_name: 'Shoes', item_description: 'Gucci mules', price: '$99', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/gucciShoes.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/gucciShoes.jpg")
 item.save
 
 
@@ -30,16 +30,16 @@ sale = Sale.create!(street: '704 J Street', city: 'San Diego', state: 'Californi
 
 
 item = Item.new(item_name: 'Saw', item_description: 'Power saw from Home Depot', price: '$60', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/saw.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/saw.jpg")
 item.save
 
 item = Item.new(item_name: 'Sander', item_description: 'Deck XP', price: '$75', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/sander.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/sander.jpg")
 item.save
 
 
 item = Item.new(item_name: 'Comfy chair', item_description: 'Plush red chair', price: '$105', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/redchair.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/redchair.jpg")
 item.save
 
 
@@ -52,7 +52,7 @@ sale = Sale.create!(street: '4711 Timberline Drive', city: 'Austin', state: 'Tex
 
 
 item = Item.new(item_name: 'Large sectional couch', item_description: 'Beautiful and comfortable L-shaped blue couch', price: '$499 or best offer', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/couchsectional.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/couchsectional.jpg")
 item.save
 
 
@@ -61,7 +61,7 @@ item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/bluechair.jpg")
 item.save
 
 item = Item.new(item_name: 'Running shoes', item_description: 'Womens size 7 black Nike running shoes', price: 'A steal at $29', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/blacknike.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/blacknike.jpg")
 item.save
 
 
@@ -70,19 +70,19 @@ user = User.create!(email: 'rob@gmail.com', password: 'jstreet', password_confir
 sale = Sale.create!(street: '1533 S Main Street', city: 'Winston-Salem', state: 'North Carolina', zip: '27127', date: '09/15/2017', description: 'Cute clothes and other items', user_id: user.id )
 
 item = Item.new(item_name: 'Overalls', item_description: 'Black Madewell overalls size extra small', price: '$49', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/overallsxs.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/overallsxs.jpg")
 item.save
 
 item = Item.new(item_name: 'Patio furniture', item_description: 'Table and 6 chairs, beige fabric', price: '$49', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/patiofurniture.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/patiofurniture.jpg")
 item.save
 
 item = Item.new(item_name: 'Russian dolls', item_description: 'Nested Russian dolls', price: '$29', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/russiandolls.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/russiandolls.jpg")
 item.save
 
 item = Item.new(item_name: 'Yellow couch', item_description: 'Low back couch with classic yellow fabric', price: '$29', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/yellowcouch.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/yellowcouch.jpg")
 item.save
 
 
@@ -91,19 +91,16 @@ user = User.create!(email: 'joe@gmail.com', password: 'jstreet', password_confir
 sale = Sale.create!(street: '22 Merrill Rd', city: 'Freeport', state: 'Maine', zip: '04032', date: '08/26/2017', description: 'Sports equipment, golf clubs and more', user_id: user.id )
 
 item = Item.new(item_name: 'Golf clubs', item_description: 'Great clubs barely used', price: '$51', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/golfclubs.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/golfclubs.jpg")
 item.save
 
 item = Item.new(item_name: 'Bookcase for girls room', item_description: 'Pottery Barn white bookcase', price: '$99', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/girlsbookcase.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/girlsbookcase.jpg")
 item.save
 
 item = Item.new(item_name: 'Baseball bat', item_description: 'Easton brand', price: '$49', sale_id: sale.id)
-item.image = File.open("/Users/amydyson/Desktop/SeedFileImages/baseballbat
-	.jpg")
+item.image = File.open("#{Rails.root}/test/SeedFileImages/baseballbat.jpg")
 item.save
-
-
 
 
 
