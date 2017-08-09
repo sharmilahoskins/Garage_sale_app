bfbf6774556e7d4c989450d122440459714886c3
oukrithy <oukrithy@yahoo.com>
2017-08-09 09:45:02 -0700

    updated edit validation for sales and items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
 
   # GET /items/new
   def new
-    @item = Item.new
+    @item = @sale.items.create(item_params)
   end
 
   # GET /items/1/edit
@@ -34,15 +34,25 @@ class ItemsController < ApplicationController
   # POST /items.json
   def create
     @sale = Sale.find(params[:sale_id])
-    @item = @sale.items.create(item_params)
+    if item_params[:item_name].empty? || item_params[:item_name].blank?
+      redirect_to @sale, notice: "Item Name can't be blank."
+    elsif item_params[:item_description].empty? || item_params[:item_description].blank?
+      redirect_to @sale, notice: "Item description can't be blank."
+    elsif item_params[:price].empty? || item_params[:price].blank?
+      redirect_to @sale, notice: "Item Price can't be blank."
+    elsif item_params[:image].nil? || item_params[:image].blank?
+      redirect_to @sale, notice: "Item Image can't be blank."
+    else
+      @item = @sale.items.create(item_params)
 
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to sale_path(@sale), notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
+      respond_to do |format|
+        if @item.save
+          format.html { redirect_to @sale, notice: 'Item was successfully created.' }
+          format.json { render :show, status: :created, location: @item }
+        else
+          format.html { redirect_to @sale, notice: 'Item was not able to be created.'  }
+          format.json { render json: @item.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -53,7 +63,7 @@ class ItemsController < ApplicationController
     @sale = Sale.find_by_id(@item.sale_id)
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to sale_items_path(@sale), notice: 'Item was successfully updated.' }
+        format.html { redirect_to @sale, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -83,4 +93,6 @@ class ItemsController < ApplicationController
     def item_params
       params.require(:item).permit(:item_name, :item_description, :price, :sale_id, :image)
     end
+
+
 end
