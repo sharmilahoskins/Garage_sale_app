223433445d7b1d8bd6f51d2bfc56c63419ee0a40
sharmila <sharmilahoskins@gmail.com>
2017-08-11 12:11:20 -0700

    map hides when you search for items

--- app/assets/javascripts/sales.js
+++ app/assets/javascripts/sales.js
@@ -7,39 +7,8 @@ $(document).ready(function(){
     $('#notice').remove();
   }, 3000);
 
+  
 
-   
-  //     // Data to be submitted
-  //     newItem = {
-  //       "item": {
-  //         "item_name": $("#item_item_name").val(),
-  //         "item_description": $("#item_item_description").val(),
-  //         "price": $("#item_price").val(),
-  //         "image": $("#attach").val(),
-  //         "sale_id": $("#sale_id").val()
-  //       }
-  //     }
-  //
-  //     $.ajax({
-  //       dataType: 'json',
-  //       url: '/items',
-  //       method: 'POST',
-  //       data: newItem,
-  //       success: function(dataFromServer) {
-  //         add_to_wine_list(dataFromServer);
-  //       },
-  //       error: function(jqXHR, textStatus, errorThrown) {
-  //         alert("Add new wine failed: " + errorThrown);
-  //       }
-  //     });
-  // });// end add wine
+
 
 }); // end document ready
-
-// Function to be called after data has been successfully submitted
-// function add_to_wine_list() {
-//   // $("#wine_list").append(
-//   //   '<li><a href="' + '/wines/' + data.id + '">' +
-//   //   data.name + "</a></li>");
-//
-// }
