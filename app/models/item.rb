b71b423bf9891e3d8978a3b2262cd16912da5188
Sharmila Hoskins <sharmilahoskins@Sharmilas-MacBook.local>
2017-08-13 10:23:44 -0700

    styled the rest of the forms

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,5 @@
 class Item < ApplicationRecord
 
-
-
-
-
   belongs_to :sale
   has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200#" }
   validates_attachment :image, presence: true,
