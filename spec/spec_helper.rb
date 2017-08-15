dcd9fa500392b4ed4baffb68d8ef7df6d5f07558
Amy Dyson <amy.dyson@mac.com>
2017-08-15 10:12:36 -0700

    Changes favicon to sign with post

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,13 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+before(:each) do
+
+  load Rails.root + "db/seeds.rb" 
+
+end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
